1ebef38b5c30b3e4b8bb0cdb44e26283be612d1e
seokhyun <letushyun@gmail.com>
2023-02-24 15:06:46 +0900

    Enum

---
+++ SingSing/SingSing/Configuration/Constant/Enum/Category.swift
@@ -0,0 +1,12 @@
+//
+//  Category.swift
+//  SingSing
+//
+//  Created by SeokHyun on 2023/02/21.
+//
+
+import UIKit
+
+//enum Category {
+//  case
+//}
---
+++ SingSing/SingSing/Configuration/Constant/Enum/Font.swift
@@ -0,0 +1,15 @@
+//
+//  Font.swift
+//  SingSing
+//
+//  Created by SeokHyun on 2023/02/22.
+//
+
+import UIKit
+
+//enum Font {
+//  static let name = UIFont.boldSystemFont(ofSize: 14)
+//  static let bioLabel = UIFont.boldSystemFont(ofSize: 12)
+//  
+//}
+//
