bdf4d8deac815b91f15574ee19d96e966c49cd67
seokhyun <letushyun@gmail.com>
2023-02-24 14:55:50 +0900

    Core Data Entity

---
+++ SingSing/SingSing/Source/Model/CoreData/Food+/Food+CoreDataClass.swift
@@ -0,0 +1,14 @@
+//
+//  Food+CoreDataClass.swift
+//  SingSing
+//
+//  Created by SeokHyun on 2023/02/21.
+//
+//
+
+import Foundation
+import CoreData
+
+public class Food: NSManagedObject {
+
+}
---
+++ SingSing/SingSing/Source/Model/CoreData/Food+/Food+CoreDataProperties.swift
@@ -0,0 +1,29 @@
+//
+//  Food+CoreDataProperties.swift
+//  SingSing
+//
+//  Created by SeokHyun on 2023/02/21.
+//
+//
+
+import Foundation
+import CoreData
+
+extension Food {
+  
+  @nonobjc public class func fetchRequest() -> NSFetchRequest<Food> {
+    return NSFetchRequest<Food>(entityName: "Food")
+  }
+  
+  @NSManaged public var uuid: UUID
+  @NSManaged public var name: String
+  @NSManaged public var expirationDate: String
+  @NSManaged public var category: String
+  @NSManaged public var extraDescription: String?
+  @NSManaged public var consumptionDate: String?
+  @NSManaged public var thumbnail: Data?
+}
+
+extension Food : Identifiable {
+  
+}
