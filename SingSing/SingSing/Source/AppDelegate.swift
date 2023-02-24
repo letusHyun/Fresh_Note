7a93cb7f7456fb940dd775eef5de45f448d2103e
seokhyun <letushyun@gmail.com>
2023-02-24 14:49:26 +0900

    초기 설정

---
+++ SingSing/SingSing/Source/AppDelegate.swift
@@ -0,0 +1,34 @@
+//
+//  AppDelegate.swift
+//  SingSing
+//
+//  Created by SeokHyun on 2023/02/13.
+//
+
+import UIKit
+import CoreData
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    return true
+  }
+  
+  // MARK: UISceneSession Lifecycle
+  
+  func application(
+    _ application: UIApplication,
+    configurationForConnecting connectingSceneSession: UISceneSession,
+    options: UIScene.ConnectionOptions
+  ) -> UISceneConfiguration {
+    return UISceneConfiguration(
+      name: "Default Configuration",
+      sessionRole: connectingSceneSession.role
+    )
+  }
+}
+
---
+++ SingSing/SingSing/Source/SceneDelegate.swift
@@ -0,0 +1,38 @@
+//
+//  SceneDelegate.swift
+//  SingSing
+//
+//  Created by SeokHyun on 2023/02/13.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+  
+  var window: UIWindow?
+  
+  func scene(
+    _ scene: UIScene,
+    willConnectTo session: UISceneSession,
+    options connectionOptions: UIScene.ConnectionOptions
+  ) {
+    guard let windowScene = (scene as? UIWindowScene) else { return }
+    window = UIWindow(windowScene: windowScene)
+    
+//    let homeVC = HomeViewController()
+//    homeVC.view.backgroundColor = UIColor(hex: "FBF8F3")
+//    let navigationController = UINavigationController(rootViewController: homeVC)
+    
+//    window?.rootViewController = navigationController
+    window?.rootViewController = MainTabBarController()
+    window?.makeKeyAndVisible()
+  }
+  
+  func sceneDidEnterBackground(_ scene: UIScene) {
+    
+    // Save changes in the application's managed object context when the application transitions to the background.
+//    (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+    PersistenceManager.shared.saveContext()
+  }
+}
+
