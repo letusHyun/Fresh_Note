e9ff4185c627c49c4a289b32a99e72035f60227e
seokhyun <letushyun@gmail.com>
2023-02-24 14:56:33 +0900

    custom TextField

---
+++ SingSing/SingSing/Source/Scene/Common/UnderlineTextField.swift
@@ -0,0 +1,69 @@
+//
+//  UnderLineTextField.swift
+//  SingSing
+//
+//  Created by SeokHyun on 2023/02/16.
+//
+
+import UIKit
+import SnapKit
+
+class UnderlineTextField: UITextField {
+  
+  // MARK: - Properties
+  private let underlineView: UIView = {
+    let view = UIView()
+    view.backgroundColor = SSType.lv1.color
+    return view
+  }()
+  
+  // MARK: - LifeCycle
+  init(placeholder: String) {
+    super.init(frame: .zero)
+    setupLayouts()
+    setupConstraints()
+    setupStyles(placeholder: placeholder)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  
+  // MARK: - Setup
+  private func setupLayouts() {
+    self.addSubview(underlineView)
+  }
+  
+  private func setupConstraints() {
+    underlineView.snp.makeConstraints {
+      $0.top.equalTo(self.snp.bottom)
+      $0.left.right.equalToSuperview()
+      $0.height.equalTo(1)
+    }
+  }
+  
+  private func setupStyles(placeholder: String) {
+    self.font = .systemFont(ofSize: 15)
+    self.textColor = .black
+    self.clearButtonMode = .whileEditing
+    let attr: [NSAttributedString.Key : Any] = [
+      .foregroundColor : SSType.placeholder.color,
+    ]
+    let attrString = NSAttributedString(
+      string: placeholder,
+      attributes: attr
+    )
+    self.attributedPlaceholder = attrString
+    
+    addLeftPadding()
+  }
+  
+  private func addLeftPadding(width: CGFloat = 5) {
+    let paddingView = UIView(
+      frame: CGRect(x: 0, y: 0, width: width, height: self.frame.height)
+    )
+    self.leftView = paddingView
+    self.leftViewMode = .always
+  }
+}
