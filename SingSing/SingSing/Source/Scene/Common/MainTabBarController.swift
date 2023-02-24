dad8ecbb8d36846012c6ada0e9ada8cdb2f5979c
seokhyun <letushyun@gmail.com>
2023-02-24 14:47:33 +0900

    TabBarController
    홈, 캘린더, 핀, 마이 setting

---
+++ SingSing/SingSing/Source/Scene/Common/MainTabBarController.swift
@@ -0,0 +1,110 @@
+//
+//  MainTabBarController.swift
+//  SingSing
+//
+//  Created by SeokHyun on 2023/02/14.
+//
+
+import UIKit
+import SnapKit
+
+class MainTabBarController: UITabBarController {
+  // MARK: - Properties
+  
+  // MARK: - LifeCycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    self.delegate = self
+    
+    setupStyles()
+    configureTabBar()
+    setupLayouts()
+    setupConstraints()
+  }
+  
+  // MARK: - Setup
+  private func setupLayouts() {
+    
+  }
+  
+  private func setupConstraints() {
+    
+  }
+  
+  private func setupStyles() {
+    
+  }
+  
+  // MARK: - Privates
+  private func configureTabBar() {
+    configureItems()
+    
+    //UITabBarAppearance
+    let appearance = UITabBarAppearance()
+    appearance.backgroundColor = SSType.background.color
+    appearance.shadowColor = .clear
+    appearance
+      .stackedLayoutAppearance
+      .selected
+      .titleTextAttributes = [
+        .foregroundColor : SSType.lv3.color,
+      ]
+    
+    tabBar.standardAppearance = appearance
+    
+    tabBar.clipsToBounds = true
+    tabBar.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+    tabBar.layer.cornerRadius = (tabBar.frame.height + 10) * 0.30
+  }
+  
+  private func configureItems() {
+    //홈
+    let homeVC = HomeViewController()
+    let homeNavi = UINavigationController(rootViewController: homeVC)
+    let homeItem = UITabBarItem(
+      title: "홈",
+      image: UIImage(systemName: "house"),
+      selectedImage: UIImage(systemName: "house.fill")?
+        .withTintColor(SSType.lv3.color, renderingMode: .alwaysOriginal)
+    )
+    homeVC.tabBarItem = homeItem
+
+    //캘린더
+    let calendarVC = CalendarViewController()
+    let calendarItem = UITabBarItem(
+      title: "캘린더",
+      image: UIImage(systemName: "calendar"),
+      selectedImage: UIImage(systemName: "calendar")?
+        .withTintColor(SSType.lv3.color, renderingMode: .alwaysOriginal)
+    )
+    calendarVC.tabBarItem = calendarItem
+    
+    //핀
+    let PinVC = PinViewController()
+    let PinItem = UITabBarItem(
+      title: "핀",
+      image: UIImage(systemName: "pin"),
+      selectedImage: UIImage(systemName: "pin.fill")?
+        .withTintColor(SSType.lv3.color, renderingMode: .alwaysOriginal)
+    )
+    PinVC.tabBarItem = PinItem
+    
+    //마이
+    let myVC = MyViewController()
+    let myItem = UITabBarItem(
+      title: "마이",
+      image: UIImage(systemName: "person"),
+      selectedImage: UIImage(systemName: "person.fill")?
+        .withTintColor(SSType.lv3.color, renderingMode: .alwaysOriginal)
+    )
+    myVC.tabBarItem = myItem
+    
+    self.viewControllers = [homeNavi, calendarVC, PinVC, myVC]
+  }
+}
+
+extension MainTabBarController: UITabBarControllerDelegate {
+  
+}
+
