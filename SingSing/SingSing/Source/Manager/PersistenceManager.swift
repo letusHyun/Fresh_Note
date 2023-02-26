2161bdafdb5a00fdf2d229aabf62e15d90e36c59
seokhyun <letushyun@gmail.com>
2023-02-27 00:14:45 +0900

    Food type으로 고정

--- SingSing/SingSing/Source/Manager/PersistenceManager.swift
+++ SingSing/SingSing/Source/Manager/PersistenceManager.swift
@@ -5,10 +5,13 @@
 //  Created by SeokHyun on 2023/02/13.
 //
 
-import Foundation
 import CoreData
+import Foundation
+
 class PersistenceManager {
+  
   // MARK: - Properties
+  
   static var shared: PersistenceManager = PersistenceManager()
   
   lazy var persistentContainer: NSPersistentContainer = {
@@ -27,38 +30,66 @@ class PersistenceManager {
     return self.persistentContainer.viewContext
   }
   
+  var entity: NSEntityDescription? {
+    return NSEntityDescription.entity(forEntityName: "Food", in: self.context)
+  }
+  
+  var fetchRequest: NSFetchRequest<Food> {
+    return Food.fetchRequest()
+  }
+  
   // MARK: - LifeCycle
+  
   private init() { }
-
+  
   // MARK: - Fetch
-  func fetch<T: NSManagedObject>(request: NSFetchRequest<T>) -> [T] {
+  
+  func fetchFoods() -> [Food] {
     do {
-      let fetchResult = try self.context.fetch(request)
-      return fetchResult
+      let results = try context.fetch(fetchRequest)
+      
+      return results
     } catch {
       print("DEBUG: \(error.localizedDescription)")
-      return []
     }
+    return []
+  }
+  
+  func fetchFoodModels() -> [FoodModel] {
+    let foods = fetchFoods()
+    var foodModels = [FoodModel]()
+    
+    for food in foods {
+      foodModels.append(FoodModel(
+        uuid: food.uuid,
+        name: food.name,
+        expirationDate: food.expirationDate,
+        consumptionDate: food.consumptionDate,
+        extraDescription: food.extraDescription,
+        category: food.category,
+        thumbnail: food.thumbnail
+      ))
+    }
+    return foodModels
   }
   
   // MARK: - Insert
+  
   @discardableResult
-  func insertItem(item: FoodModel) -> Bool {
+  func insertFood(foodModel: FoodModel) -> Bool {
     // 2. Entity를 가져온다
-    let entity = NSEntityDescription.entity(forEntityName: "Food", in: self.context) // Food
-    
-    if let entity = entity {
+    if let entity = self.entity {
       // 3. NSManagedObject를 만든다. (Entity로 형변환)
       let object = NSManagedObject(entity: entity, insertInto: self.context)
-
-        // 4. NSManagedObject에 값을 세팅한다.
-      object.setValue(item.name, forKey: "name")
-      object.setValue(item.uuid, forKey: "uuid")
-      object.setValue(item.expirationDate, forKey: "expirationDate")
-      object.setValue(item.consumptionDate, forKey: "consumptionDate")
-      object.setValue(item.extraDescription, forKey: "extraDescription")
-      object.setValue(item.category, forKey: "category")
-      object.setValue(item.thumbnail, forKey: "thumbnail")
+      
+      // 4. NSManagedObject에 값을 세팅한다.
+      object.setValue(foodModel.name, forKey: "name")
+      object.setValue(foodModel.uuid, forKey: "uuid")
+      object.setValue(foodModel.expirationDate, forKey: "expirationDate")
+      object.setValue(foodModel.consumptionDate, forKey: "consumptionDate")
+      object.setValue(foodModel.extraDescription, forKey: "extraDescription")
+      object.setValue(foodModel.category, forKey: "category")
+      object.setValue(foodModel.thumbnail, forKey: "thumbnail")
       
       // 5. [최종] NSManagedObjectContext에 저장한다.
       do {
@@ -73,7 +104,34 @@ class PersistenceManager {
     }
   }
   
+  @discardableResult
+  func editFood(foodModel: FoodModel) -> Bool {
+    
+    let foods = PersistenceManager.shared.fetchFoods()
+    
+    for selectedFood in foods {
+      if selectedFood.uuid == foodModel.uuid {
+        selectedFood.name = foodModel.name
+        selectedFood.expirationDate = foodModel.expirationDate
+        selectedFood.consumptionDate = foodModel.consumptionDate
+        selectedFood.category = foodModel.category
+        selectedFood.extraDescription = foodModel.extraDescription
+        selectedFood.thumbnail = foodModel.thumbnail
+      }
+    }
+    
+    //[최종] NSManagedObjectContext에 저장한다.
+    do {
+      try self.context.save()
+      return true
+    } catch {
+      print("DEBUG: \(error.localizedDescription)")
+      return false
+    }
+  }
+  
   // MARK: - Delete
+  
   @discardableResult
   func delete(object: NSManagedObject) -> Bool {
     self.context.delete(object)
@@ -85,6 +143,7 @@ class PersistenceManager {
     }
   }
   
+  // TODO: - 제네릭 수정
   @discardableResult
   func deleteAll<T: NSManagedObject>(request: NSFetchRequest<T>) -> Bool {
     let request: NSFetchRequest<NSFetchRequestResult> = T.fetchRequest()
@@ -97,17 +156,8 @@ class PersistenceManager {
     }
   }
   
-  // MARK: - Count
-  func count<T: NSManagedObject>(request: NSFetchRequest<T>) -> Int? {
-    do {
-      let count = try self.context.count(for: request)
-      return count
-    } catch {
-      return nil
-    }
-  }
-  
   //  // MARK: - Save
+  
   func saveContext () {
     if context.hasChanges {
       do {
